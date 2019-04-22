650dacad5e92c2b30c7fa42328f326f2d87b4497
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-22 05:50:53 -0500

    Update main.cpp

--- NumberList/main.cpp
+++ NumberList/main.cpp
@@ -1,8 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include "NumberList.h"
 using namespace std;
 
 int main()
 {
-	 
+	NumberList list;
+	
+	NumberList listA, listB;
+	
+	listA.appendNode(10);
+	listA.appendNode(20);
+	listB.appendNode(15);
+	listB.appendNode(40);
+	
+	cout<< " listA before assignment: "<< endl;
+	listA.displayList();
+	
+	cout<< "listB before assignment: " << endl;
+	listB.displayList();
+	
+	listA = listB;
+	listB.appendNode(50);
+	
+	cout << listA after assignment: " << endl;
+	listA.displayList();
+	
+	cout << "listB after assignment: " << endl;
+	listB.displayList();
+	
+	//Tsk5
+	
+	listB.appendNode(20);
+	listB.appendNode(30);
+	
+	
+	listB.deleteNode(15);
+	listB.deleteNode(30);
+	
+	listB.deleteNode(50);
+	
 	return 0;
 }
